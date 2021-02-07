5cd891dd22e56917e427552a88238cad2d2e9884
Cyberlin <1242546625@qq.com>
2021-02-07 20:26:36 +0800

    add the README file

---
+++ README.md
@@ -0,0 +1,5 @@
+##git is so complicated 
++ I have read a lot tutorial 
++ Different tools have different layout for git 
++ I have never engaged in a real team work with git 
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main() {
+    int v[1000];
+    int w[1000];
+    int n ;
+    int W;
+    cin>>n>>W;
+
+    for (int i =0;i<n;i++){
+        for(int j =0;j<W;j++){
+            ;
+        }
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
